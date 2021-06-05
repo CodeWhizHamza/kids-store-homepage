5440e300c9a85dad20d8d84e90f0cd11c1998625
Hamza <muhammadhamzam1486@gmail.com>
2021-06-05 14:36:14 +0500

    DESIGN all sections except last

--- js/app.js
+++ js/app.js
@@ -2,26 +2,8 @@
 const btnToggler = document.querySelector(".btn--toggler");
 
 // Functions
-const runSwiper = function (swiper) {
-  return new Swiper(swiper, {
-    centeredSlides: true,
-    grabCursor: true,
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const runSwiper = function (swiper, options) {
+  return new Swiper(swiper, options);
 };
 const toggleMenu = function () {
   const lists = document.querySelectorAll(".list.list--row");
@@ -29,5 +11,53 @@ const toggleMenu = function () {
 };
 
 // Calls
-const homeSwiper = runSwiper(".homeSwiper");
+const homeSwiper = runSwiper(".homeSwiper", {
+  grabCursor: true,
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const productsSwiper = runSwiper(".productsSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    400: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 btnToggler.addEventListener("click", toggleMenu);
